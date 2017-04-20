324af4c870527f76ccb993b9402dbf712d11a870
Hartono Halim <gopher@equalbyte.com>
2017-04-20 13:35:21 -0500

    Update to Shift 1.0.7.0

--- Program.cs
+++ Program.cs
@@ -43,21 +43,27 @@ namespace Shift.WebJob
             var config = new Shift.ServerConfig();
             config.AssemblyFolder = ConfigurationManager.AppSettings["AssemblyFolder"];
             //config.AssemblyListPath = ConfigurationManager.AppSettings["AssemblyListPath"];
-            config.MaxRunnableJobs = Convert.ToInt32(ConfigurationManager.AppSettings["MaxRunableJobs"]);
+            config.MaxRunnableJobs = Convert.ToInt32(ConfigurationManager.AppSettings["MaxRunnableJobs"]);
             config.ProcessID = ConfigurationManager.AppSettings["ShiftPID"];
             config.DBConnectionString = ConfigurationManager.ConnectionStrings["ShiftDBConnection"].ConnectionString;
+            config.Workers = Convert.ToInt32(ConfigurationManager.AppSettings["ShiftWorkers"]);
 
             config.ServerTimerInterval = Convert.ToInt32(ConfigurationManager.AppSettings["TimerInterval"]); //optional: default every 5 sec for getting jobs ready to run and run them
             config.ServerTimerInterval2 = Convert.ToInt32(ConfigurationManager.AppSettings["CleanUpTimerInterval"]); //optional: default every 10 sec for server CleanUp()
-
-            var autoDeletePeriod = ConfigurationManager.AppSettings["AutoDeletePeriod"];
-            config.AutoDeletePeriod = string.IsNullOrWhiteSpace(autoDeletePeriod) ? null : (int?)Convert.ToInt32(autoDeletePeriod);
-            //config.AutoDeleteStatus = new List<JobStatus?> { JobStatus.Completed, JobStatus.Error }; //Auto delete only the jobs that had Stopped or with Error
 
             config.StorageMode = ConfigurationManager.AppSettings["StorageMode"];
             var progressDBInterval = ConfigurationManager.AppSettings["ProgressDBInterval"];
             if (!string.IsNullOrWhiteSpace(progressDBInterval))
                 config.ProgressDBInterval = TimeSpan.Parse(progressDBInterval); //Interval when progress is updated in main DB
+
+            var autoDeletePeriod = ConfigurationManager.AppSettings["AutoDeletePeriod"];
+            config.AutoDeletePeriod = string.IsNullOrWhiteSpace(autoDeletePeriod) ? null : (int?)Convert.ToInt32(autoDeletePeriod);
+            //config.AutoDeleteStatus = new List<JobStatus?> { JobStatus.Completed, JobStatus.Error }; //Auto delete only the jobs that had completed or with error.
+
+            config.ForceStopServer = Convert.ToBoolean(ConfigurationManager.AppSettings["ForceStopServer"]); //Set to true to allow windows service to shut down after a set delay in StopServerDelay
+            config.StopServerDelay = Convert.ToInt32(ConfigurationManager.AppSettings["StopServerDelay"]);
+
+            config.PollingOnce = Convert.ToBoolean(ConfigurationManager.AppSettings["PollingOnce"]);
 
             //config.UseCache = Convert.ToBoolean(ConfigurationManager.AppSettings["UseCache"]);
             //config.CacheConfigurationString = ConfigurationManager.AppSettings["RedisConfiguration"];
